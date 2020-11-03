569cd6b170294a846afe681d72c1c75bac6ae916
Alexander Kim <alkim@Alexanders-MacBook-Pro.local>
2020-11-03 19:41:09 +0600

    added torch.load to constructor

--- models/generator_model.py
+++ models/generator_model.py
@@ -5,7 +5,7 @@ from networks.Generator import Generator
 class GeneratorModel(object):
     def __init__(self, gen_weights_path):
         self.gen = Generator()
-        self.gen.load_state_dict(gen_weights_path)
+        self.gen.load_state_dict(torch.load(gen_weights_path))
 
     def generate(self, n_samples):
         return self.gen(self.gen.gen_noize(n_samples))
