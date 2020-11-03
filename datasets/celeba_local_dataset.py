4a24f1bf0ff79b22d2814ee042c8bde57ed6b717
Alexander Kim <alkim@Alexanders-MacBook-Pro.local>
2020-11-03 14:38:35 +0600

    using batch_size parameter instead of ignoring it

--- datasets/celeba_local_dataset.py
+++ datasets/celeba_local_dataset.py
@@ -5,7 +5,7 @@ from torchvision import transforms
 from torch.utils.data import DataLoader
 
 
-def load_dataset(path, batch_size):
+def load_dataset(path, batch_size=128):
     dataset_transforms = transforms.Compose([
         transforms.Resize(64),
         transforms.CenterCrop(64),
@@ -13,6 +13,6 @@ def load_dataset(path, batch_size):
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
     dataset = torchvision.datasets.ImageFolder(root=path, transform=dataset_transforms)
-    data_loader = DataLoader(dataset=dataset, batch_size=128)
+    data_loader = DataLoader(dataset=dataset, batch_size=batch_size)
 
     return data_loader
